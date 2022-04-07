7a53a697b665e83e72959a64134407cb7a79a053
rakibshakib <rakibshakib74@gmail.com>
2022-04-08 01:29:50 +0600

    update structure

--- src/redux/reducers/peopleReducer.js
+++ src/redux/reducers/peopleReducer.js
@@ -2,27 +2,37 @@ import peoples from '../../Data/people-data.json';
 const initialState = {
     peoples: peoples,
     addedPeople: [],
-    removedPeople: []
-}
+    removedPeople: [],
+};
 
 function peopleReducer(state = initialState, action) {
-    console.log("this is the action", action.payload)
+    console.log('this is the action', action.payload);
     switch (action.type) {
-        case "ADD_PEOPLE_TO_LIST": {
-            const isExist = state.addedPeople.find(item=> item.id === action.payload.id)
-            if(isExist){
+        case 'ADD_PEOPLE_TO_LIST': {
+            const isExist = state.addedPeople.find(
+                (item) => item.id === action.payload.id
+            );
+            if (isExist) {
                 return state;
             }
-            const newState = { ...state, addedPeople: [...state.addedPeople, action.payload] };
-            return newState
+            const newState = {
+                ...state,
+                addedPeople: [...state.addedPeople, action.payload],
+            };
+            return newState;
         }
-        case "REMOVE_PEOPLE_FROM_LIST": {
-            const newState = { ...state, addedPeople: state.addedPeople.filter(people => people.id !== action.payload.id), removedPeople: [...state.removedPeople, action.payload] };
-            return newState
+        case 'REMOVE_PEOPLE_FROM_LIST': {
+            const newState = {
+                ...state,
+                addedPeople: state.addedPeople.filter(
+                    (people) => people.id !== action.payload.id
+                ),
+                removedPeople: [...state.removedPeople, action.payload],
+            };
+            return newState;
         }
         default:
             return state;
     }
-
 }
 export default peopleReducer;
